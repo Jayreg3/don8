02db81802049ca3f5cb9942ec1ee6739ffa40347
erin <erin.hardnett.7@gmail.com>
2019-05-23 19:45:48 -0400

    Created the firebase authentication and database

--- app/src/main/java/com/example/don8/RestaurantSignUpActivity.java
+++ app/src/main/java/com/example/don8/RestaurantSignUpActivity.java
@@ -1,13 +1,27 @@
 package com.example.don8;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RestaurantSignUpActivity extends AppCompatActivity {
 
@@ -29,10 +43,18 @@ public class RestaurantSignUpActivity extends AppCompatActivity {
     private RadioButton no;
     private Button create;
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+       firebaseAuth = FirebaseAuth.getInstance();
+       firebaseDatabase = FirebaseDatabase.getInstance();
+
 
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
@@ -55,7 +77,20 @@ public class RestaurantSignUpActivity extends AppCompatActivity {
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String inputtedEmail = email.getText().toString().trim();
+                String inputtedPassword = password.getText().toString();
 
+                firebaseAuth.createUserWithEmailAndPassword(inputtedEmail, inputtedPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Toast.makeText(RestaurantSignUpActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RestaurantSignUpActivity.this, "Couldn't create user", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
             }
         });
     }
