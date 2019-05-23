aa4acb3fcb8670351a9d053a01428cb0858f39dd
LaDawnaRM <lamcenhimer@gmail.com>
2019-05-23 16:23:44 -0400

    profile page basic functionality

--- app/src/main/java/com/example/don8/ProfileActivity.java
+++ app/src/main/java/com/example/don8/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.don8;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import java.util.List;
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView profile_name;
+    private TextView company_id;
     private TextView num_donations;
     private TextView donation_value;
     private ProgressBar progressBar;
@@ -22,6 +24,7 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView liability_link_two;
     private TextView liability_link_three;
     private TextView contact_number;
+    private Boolean isInvisible = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.profile);
 
         profile_name = findViewById(R.id.profile_name);
+        company_id = findViewById(R.id.company_id);
         contact_us = findViewById(R.id.contact_us);
         donation_value = findViewById(R.id.donation_value);
         progressBar = findViewById(R.id.progressBar);
@@ -36,12 +40,28 @@ public class ProfileActivity extends AppCompatActivity {
         contact_us = findViewById(R.id.contact_us);
 
         liability_link_one = findViewById(R.id.liability_link_one);
-        liability_link_one = findViewById(R.id.liability_link_two);
+        liability_link_two = findViewById(R.id.liability_link_two);
         liability_link_three = findViewById(R.id.liability_link_three);
         contact_number = findViewById(R.id.contact_number);
 
+        liability_laws.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(liability_link_one.getVisibility() == View.INVISIBLE){
+                    liability_link_one.setVisibility(View.VISIBLE);
+                    liability_link_two.setVisibility(View.VISIBLE);
+                    liability_link_three.setVisibility(View.VISIBLE);
+                    isInvisible=false;
+                }
+                else{
+                    liability_link_one.setVisibility(View.INVISIBLE);
+                    liability_link_two.setVisibility(View.INVISIBLE);
+                    liability_link_three.setVisibility(View.INVISIBLE);
+                    isInvisible=true;
+                }
+            }
+        });
 
     }
 
-    
 }
